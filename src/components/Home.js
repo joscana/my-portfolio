db011760a11a92e5a7c9808124bae9bffa689c62
Joscana Garcia <Joscana@users-MacBook-Pro.local>
2021-05-11 11:44:45 -0400

    beginning design

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home(props) {
+    return (
+        <div className="Home">
+            <h1 className="Name">Joscana Garcia</h1>
+            <h2 className="Position">Front End Web Developer</h2>
+        </div>
+    )
+}
+
+export default Home;
