647f092578002bc18ee1bd2183d28d1125e4eca3
Joscana Garcia <Joscana@users-MacBook-Pro.local>
2021-06-16 14:18:44 -0400

    expanding and collapsing div

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../App.scss'
 
 function Course(props) {
+    const [show, setShow] = useState(false);
+
+    function handleButtonClick() {
+        console.log('Clicked!')
+        setShow(!show)
+    }
+
     return (
         <div className="Course-container">
             <h1>{props.title}</h1>
+            <button type="button" class="button" onClick={handleButtonClick}>
+                {show ? "Collapse" : "Expand" }
+            </button>
             <ul className='course-bullets'>
                 {props.bullets.map((bullet) => {
                     return <li>{bullet}</li>
                 })}
             </ul>
-            <img className='certificate' src={props.cert} />
+            {show ? <img className='certificate' src={props.cert} /> : null}
         </div>
     )
 }
