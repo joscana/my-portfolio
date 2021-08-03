4a9d091b00a7d195a359d979884040f0a8d3b6fe
Joscana Garcia <Joscana@users-MacBook-Pro.local>
2021-08-03 11:58:17 -0400

    basic layout

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../App.scss'
+import profile from '../icons/profile-picture.png';
 
 
 
@@ -8,6 +9,7 @@ function About(props) {
         <div className="About">
             <h1> About Joscana </h1>
             <p> I am Joscana, an FAU graduate with a BA in Psychology who is studying and pursuing a career in front-end web development. I decided to make a career change from teaching to programming in 2019 and have since completed three nanodegree programs through Udacity. I have a passion for learning and I am always looking to improve and expand my skillset.</p>
+            <img className="profile" src={profile} />
         </div>
     )
 }
